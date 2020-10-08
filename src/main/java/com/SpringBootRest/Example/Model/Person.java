9926251fbb9ad0ba442b32eabc0f05cb9172f85b
Ronnie434 <56054439+Ronnie434@users.noreply.github.com>
2020-10-08 16:05:51 -0700

    MySQL DataBase added

--- src/main/java/com/SpringBootRest/Example/Model/Person.java
+++ src/main/java/com/SpringBootRest/Example/Model/Person.java
@@ -1,14 +1,23 @@
 package com.SpringBootRest.Example.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Persons")
 public class Person {
 	
 	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private String name;
+
 	private int age;
+
 	private String department;
 	
 	
@@ -21,12 +30,14 @@ public class Person {
 		this.age = age;
 		this.department = department;
 	}
+	@Column(name="Name", unique=false, nullable=false, length = 100)
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	@Column(name="Age", unique=false, nullable=true, length=100)
 	public int getAge() {
 		return age;
 	}
@@ -36,6 +47,7 @@ public class Person {
 	public String getDepartment() {
 		return department;
 	}
+	@Column(name="Department", unique=false, nullable=true, length=100)
 	public void setDepartment(String department) {
 		this.department = department;
 	}
--- src/main/java/com/SpringBootRest/Example/Repository/PersonRepository.java
+++ src/main/java/com/SpringBootRest/Example/Repository/PersonRepository.java
@@ -1,9 +1,11 @@
 package com.SpringBootRest.Example.Repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.SpringBootRest.Example.Model.Person;
 
-public interface PersonRepository extends CrudRepository<Person, String>{
+@Repository
+public interface PersonRepository extends JpaRepository<Person, String>{
 
 }
